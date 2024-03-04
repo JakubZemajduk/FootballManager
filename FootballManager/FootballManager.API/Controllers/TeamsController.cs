ca2ff48df20840a8b27cbc36d9d47a241659cac9
Jakub Żemajduk <kubazemajduk1@gmail.com>
2024-03-04 22:06:37 +0100

    Add firs endpoint

---
+++ FootballManager/FootballManager.API/Controllers/TeamsController.cs
@@ -0,0 +1,24 @@
+﻿using FootballManager.API.Persistance;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private readonly ITeamsRepository _teamsRepository;
+
+        public TeamsController(ITeamsRepository teamsRepository)
+        {
+            _teamsRepository = teamsRepository;
+        }
+
+        [HttpGet(Name = "GetTeams")]
+        public IActionResult Get()
+        {
+            return Ok(_teamsRepository.GetTeams());
+        }
+    }
+}
---
+++ FootballManager/FootballManager.API/Persistance/TeamsRepository.cs
@@ -0,0 +1,25 @@
+﻿using FootballManager.API.Models;
+
+namespace FootballManager.API.Persistance
+{
+    public interface ITeamsRepository
+    {
+        ICollection<Team> GetTeams();
+    }
+
+    public class TeamsRepository : ITeamsRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeamsRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Team> GetTeams()
+        {
+            return _context.Teams.ToList();
+        }
+
+    }
+}
