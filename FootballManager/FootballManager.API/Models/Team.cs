98e53d718ed06ab2b392f5c8bbc8027f957f0550
Jakub Żemajduk <kubazemajduk1@gmail.com>
2024-03-04 21:44:25 +0100

    First migration

---
+++ FootballManager/FootballManager.API/Models/Team.cs
@@ -0,0 +1,10 @@
+﻿namespace FootballManager.API.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ FootballManager/FootballManager.API/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FootballManager.API.Models;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballManager.API.Persistance
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = localhost; Database = FootballManagerDb; User = sa; Password = yourStrong(!)Password; Integrated Security = False; Timeout = 30;");
+        }
+    }
+}
